9971f64dcc5836a2ae935e61a8ed8c72c6b1c738
Sebastian Toledo <seba95matias@hotmail.com>
2023-01-10 22:09:29 -0300

    create CardOrder

---
+++ src/components/Atoms/CardOrder/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import Order from "../../../Type/Order";
+
+// interface Props {
+//     order: Order;
+// }
+
+const CardOrder = () => {
+  return <Flex></Flex>;
+};
+
+export default CardOrder;
