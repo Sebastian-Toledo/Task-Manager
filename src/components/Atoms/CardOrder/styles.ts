a89226e560c9e53eaeafd09bc9f657571fc37e26
Sebastian Toledo <seba95matias@hotmail.com>
2023-01-12 17:30:59 -0300

    create styles of orders

---
+++ src/components/Atoms/CardOrder/styles.ts
@@ -0,0 +1,20 @@
+const cardOrderStyles = {
+  mainContainerCard: {
+    display: "flex",
+    flexDirection: "row",
+    padding: "4",
+    gap: "4",
+    justifyContent: "space-between",
+    backgroundColor: "green",
+  },
+  flexContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "2",
+  },
+  cardContainer: {
+    backgroundColor: "yellow",
+  },
+};
+
+export default cardOrderStyles;
