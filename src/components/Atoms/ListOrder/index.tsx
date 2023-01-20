df9d292cd6f15ace00a2a81c524cdcb4cce0c73e
Sebastian Toledo <seba95matias@hotmail.com>
2023-01-20 00:19:24 -0300

    create list of Orders

--- src/components/Atoms/ListOrder/index.tsx
+++ src/components/Atoms/ListOrder/index.tsx
@@ -1,18 +1,43 @@
 import CardOrder from "../CardOrder";
 import React from "react";
-import { List, ListItem, ListIcon } from "@chakra-ui/react";
+import { List, ListItem } from "@chakra-ui/react";
+import Order from "../../../Type/Order";
+import { useState, useEffect } from "react";
 
 const ListOrder = () => {
+  const [orders, setOrders] = useState<Order[]>([]);
+  //const [stateOrders, setStateOrders] = useState(" ");
+
+  useEffect(() => {
+    fetch(`https://63c06874e262345656fe70d5.mockapi.io/api/v1/Task`)
+      .then((response) => response.json())
+      .then((order) => setOrders(order));
+  }, []);
+
+  // const filterOrders = (order: Order) => {
+  //   if (!stateOrders) {
+  //     return true;
+  //   }
+  //   return order.stateOrder
+  //     .toLowerCase()
+  //     .includes(order.stateOrder.toLowerCase());
+  // };
+
+  const renderOrder = (order: Order, index: number) => (
+    <CardOrder order={order} key={`${JSON.stringify(order)}/${index}`} />
+  );
+
+  const rederContent = () => {
+    if (!orders.length) {
+      return "hola";
+    }
+    return orders.map(renderOrder);
+  };
+
   return (
     <List backgroundColor="red" paddingBottom="2" paddingTop="2">
       <ListItem backgroundColor="gray" alignItems="center" padding="2">
-        <CardOrder />
-      </ListItem>
-      <ListItem backgroundColor="gray" alignItems="center" padding="2">
-        <CardOrder />
-      </ListItem>
-      <ListItem backgroundColor="gray" alignItems="center" padding="2">
-        <CardOrder />
+        {rederContent()}
       </ListItem>
     </List>
   );
