4a5278f9cfde46108c00c2c414ec3c50f98451b5
Sebastian Toledo <seba95matias@hotmail.com>
2023-01-22 02:57:39 -0300

    added a divider and fixed styles

--- src/components/Atoms/CardOrder/index.tsx
+++ src/components/Atoms/CardOrder/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Card, Text, Divider } from "@chakra-ui/react";
+import { Flex, Card, Text, Divider, CardBody } from "@chakra-ui/react";
 import Order from "../../../Type/Order";
 import cardOrderStyles from "./styles";
 import Routes from "../../../Router/Routes";
@@ -25,39 +25,41 @@ const CardOrder = (props: Props) => {
 
   return (
     <Link to={`${Routes.ORDER}/${id}`}>
-      <Card sx={cardOrderStyles.mainContainerCard}>
-        <Flex sx={cardOrderStyles.flexContainer}>
-          <Text>Trabajo</Text>
-          <Card sx={cardOrderStyles.cardContainer}>{title}</Card>
-        </Flex>
-        <Flex sx={cardOrderStyles.flexContainer}>
-          <Text>Autor</Text>
-          <Card sx={cardOrderStyles.cardContainer}>{author}</Card>
-        </Flex>
-        <Flex sx={cardOrderStyles.flexContainer}>
-          <Text>Fecha Ingreso</Text>
-          <Card sx={cardOrderStyles.cardContainer}>
-            {c.toLocaleDateString("en-GB")}
-          </Card>
-        </Flex>
-        <Flex sx={cardOrderStyles.flexContainer}>
-          <Text>Fecha Limite</Text>
-          <Card sx={cardOrderStyles.cardContainer}>
-            {d.toLocaleDateString("en-GB")}
-          </Card>
-        </Flex>
-        <Flex sx={cardOrderStyles.flexContainer}>
-          <Text>Estado</Text>
-          <Card sx={cardOrderStyles.cardContainer}>{stateOrder}</Card>
-        </Flex>
-        <Flex sx={cardOrderStyles.flexContainer}>
-          <Text>Empleado</Text>
-          <Card sx={cardOrderStyles.cardContainer}>{employee}</Card>
-        </Flex>
-        <Flex sx={cardOrderStyles.flexContainer}>
-          <Text>Num Presupuesto</Text>
-          <Card sx={cardOrderStyles.cardContainer}>{budget}</Card>
-        </Flex>
+      <Card>
+        <CardBody sx={cardOrderStyles.mainContainerCard}>
+          <Flex sx={cardOrderStyles.flexContainer}>
+            <Text>Titulo</Text>
+            <Card sx={cardOrderStyles.cardContainer}>{title}</Card>
+          </Flex>
+          <Flex sx={cardOrderStyles.flexContainer}>
+            <Text>Autor</Text>
+            <Card sx={cardOrderStyles.cardContainer}>{author}</Card>
+          </Flex>
+          <Flex sx={cardOrderStyles.flexContainer}>
+            <Text>Fecha Ingreso</Text>
+            <Card sx={cardOrderStyles.cardContainer}>
+              {c.toLocaleDateString("en-GB")}
+            </Card>
+          </Flex>
+          <Flex sx={cardOrderStyles.flexContainer}>
+            <Text>Fecha Limite</Text>
+            <Card sx={cardOrderStyles.cardContainer}>
+              {d.toLocaleDateString("en-GB")}
+            </Card>
+          </Flex>
+          <Flex sx={cardOrderStyles.flexContainer}>
+            <Text>Estado</Text>
+            <Card sx={cardOrderStyles.cardContainer}>{stateOrder}</Card>
+          </Flex>
+          <Flex sx={cardOrderStyles.flexContainer}>
+            <Text>Empleado</Text>
+            <Card sx={cardOrderStyles.cardContainer}>{employee}</Card>
+          </Flex>
+          <Flex sx={cardOrderStyles.flexContainer}>
+            <Text>Presupuesto</Text>
+            <Card sx={cardOrderStyles.cardContainer}>{budget}</Card>
+          </Flex>
+        </CardBody>
       </Card>
       <Divider sx={cardOrderStyles.divider} />
     </Link>
